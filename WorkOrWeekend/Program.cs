0fbb879b42e0e48d74c8d68654b9844e20cbe7ee
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-15 23:38:37 +0300

    Add task WorkOrWeekend

---
+++ WorkOrWeekend/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+WorkOrWeekend(GreetingMessage("Введите номер дня недели : "));
+
+void WorkOrWeekend(int dayNumber)
+{
+  if (dayNumber < 6 || dayNumber > 7)
+    System.Console.WriteLine("Сегодня не выходной");
+  else
+    System.Console.WriteLine("Сегодня выходной");
+
+}
+
+int GreetingMessage(string message)
+{
+  System.Console.Write(message);
+  return PutNumber();
+}
+
+int PutNumber()
+{
+  int putNumber = Convert.ToInt32(Console.ReadLine());
+  return putNumber;
+}
