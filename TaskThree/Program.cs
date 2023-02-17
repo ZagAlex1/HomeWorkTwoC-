029250392099e0113fb3e8f9990272bd31c481b9
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-17 17:48:09 +0300

    Add TaskThree

---
+++ TaskThree/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите число :");
+int[] number = new int[4];
+
+for (int i = 0; i < number.Length; i++)
+{
+  number[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+double result = Math.Sqrt((number[2] - number[0]) * (number[2] - number[0]) +
+((number[3] - number[1]) * (number[3] - number[1])));
+System.Console.WriteLine(result);
