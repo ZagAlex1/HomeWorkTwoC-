d224a1b96947bf5b332dbb02cda3dcda9cb30f83
Aleksey Zagorodnyh <zagor85@bk.ru>
2023-02-17 17:47:14 +0300

    Add TaskTwo

---
+++ TaskTwo/Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.Write("Ведите число от 1 до 4 : ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+switch (number)
+{
+  case 1:
+    System.Console.WriteLine("X > 0, Y > 0");
+    break;
+  case 2:
+    System.Console.WriteLine("X < 0, Y > 0");
+    break;
+  case 3:
+    System.Console.WriteLine("X < 0, Y < 0");
+    break;
+  case 4:
+    System.Console.WriteLine("X > 0, Y < 0");
+    break;
+  default:
+    System.Console.WriteLine("Такой координатной плоскости не существует!!!");
+    break;
+}
